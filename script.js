ae50a7e4bde653a83ddf932c741fc214fcf4a85e
jumbledworksgames-eng <jumbledworksgames@gmail.com>
2026-07-24 17:37:58 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let money = 0;
-let lemonadePower = 1;
+let power = 1;
 let upgradeCost = 10;
 
+
 function sellLemonade() {
-    money += lemonadePower;
+
+    money += power;
 
     document.getElementById("message").innerHTML =
-        "🍋 Someone bought your lemonade!";
+        "🍋 Lemonade sold!";
 
-    updateScreen();
+    update();
+
 }
 
 
@@ -17,31 +20,35 @@ function buyUpgrade() {
     if (money >= upgradeCost) {
 
         money -= upgradeCost;
-        lemonadePower += 1;
+
+        power += 1;
 
         upgradeCost *= 2;
 
+
         document.getElementById("message").innerHTML =
-            "✨ Your recipe improved!";
+            "✨ Recipe upgraded!";
 
     } else {
 
         document.getElementById("message").innerHTML =
-            "❌ You need more money!";
+            "❌ Not enough money!";
 
     }
 
-    updateScreen();
+
+    update();
+
 }
 
 
-function updateScreen() {
+
+function update() {
 
     document.getElementById("money").innerHTML = money;
 
-    document.getElementById("power").innerHTML =
-        lemonadePower;
+    document.getElementById("power").innerHTML = power;
 
-    document.getElementById("upgradeCost").innerHTML =
-        upgradeCost;
+    document.getElementById("upgradeCost").innerHTML = upgradeCost;
+
 }
